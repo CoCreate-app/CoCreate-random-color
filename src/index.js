1d29be5c1c396b0e5e4c3a53b95b31edda721bf1
Frank pagan <frank@cocreate.app>
2021-11-04 18:47:58 +0000

    feat: now works with html attribute or randomColor() function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-let defaultSaturation = 75;
-let defaultLightness = 58;
-let defaultAlpha = 1;
-
-
-function updateRandomColor(){
+function init() {
   let elements = document.querySelectorAll("[background-color='random']");
-  for(let el of elements){
-    el.style.backgroundColor = randomHSLColor();
+  initElements(elements);
+}
+
+function initElements(elements) {
+  for (let el of elements) {
+    el.style.backgroundColor = randomColor();
   }
 }
 
-function randomHSLColor(){
-  var hash = Math.floor(Math.random() * (360));
-  return 'hsla('+hash+', '+defaultSaturation+'%, '+defaultLightness+'%, '+defaultAlpha+')';
+export function randomColor() {
+  let defaultSaturation = 75;
+  let defaultLightness = 58;
+  let defaultAlpha = 1;
+  let hash = Math.floor(Math.random() * (360));
+  return 'hsla(' + hash + ', ' + defaultSaturation + '%, ' + defaultLightness + '%, ' + defaultAlpha + ')';
 }
 
-const CoCreateRandomColor = { randomHSLColor, randomHSLColor };
-export default CoCreateRandomColor;
+init();
+
+export default { randomColor };
