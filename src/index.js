43eea511b5c88b698f9db4937943a59f75a5bf96
frankpagan <frank@cocreate.app>
2025-03-31 07:08:51 -0600

    fix: update observer observe param to type and and attributeName to attributeFilter

--- src/index.js
+++ src/index.js
@@ -1,35 +1,43 @@
-import observer from '@cocreate/observer';
+import observer from "@cocreate/observer";
 
 function init() {
-    let elements = document.querySelectorAll("[background-color='random']");
-    initElements(elements);
+	let elements = document.querySelectorAll("[background-color='random']");
+	initElements(elements);
 }
 
 function initElements(elements) {
-    for (let el of elements) {
-        el.style.backgroundColor = randomColor();
-    }
+	for (let el of elements) {
+		el.style.backgroundColor = randomColor();
+	}
 }
 
 export function randomColor() {
-    let defaultSaturation = 75;
-    let defaultLightness = 58;
-    let defaultAlpha = 1;
-    let hash = Math.floor(Math.random() * (360));
-    return 'hsla(' + hash + ', ' + defaultSaturation + '%, ' + defaultLightness + '%, ' + defaultAlpha + ')';
+	let defaultSaturation = 75;
+	let defaultLightness = 58;
+	let defaultAlpha = 1;
+	let hash = Math.floor(Math.random() * 360);
+	return (
+		"hsla(" +
+		hash +
+		", " +
+		defaultSaturation +
+		"%, " +
+		defaultLightness +
+		"%, " +
+		defaultAlpha +
+		")"
+	);
 }
 
 observer.init({
-    name: 'CoCreateRandomColor',
-    observe: ['addedNodes'],
-    selector: '[background-color="random"]',
-    callback: mutation => {
-        init(mutation.target);
-    }
+	name: "CoCreateRandomColor",
+	types: ["addedNodes"],
+	selector: '[background-color="random"]',
+	callback: (mutation) => {
+		init(mutation.target);
+	}
 });
 
-
 init();
-
 
 export default { randomColor };
